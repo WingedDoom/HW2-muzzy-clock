b1498bbb50833734ed0b885521097cb1891ce4e7
Bulat Khabirov <khabiroff_b@me.com>
2017-02-25 19:15:13 +0300

    Added time parsing function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
-public class Main {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+	    String timeStr = "11:05";
+        System.out.println(parseString(timeStr));
+    }
+
+    public static int parseString(String timeString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("k:mm");
+        LocalTime time = LocalTime.parse(timeString, formatter);
+        return time.getMinute() + 60*time.getHour();
     }
 }
