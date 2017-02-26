238088507c85dbfff739ddb637d965919b6e833b
Bulat Khabirov <khabiroff_b@me.com>
2017-02-27 01:13:09 +0300

    Maximum minutes calculating method implemented (using Knapsack)

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,11 @@ import java.util.Scanner;
 public class Main {
     private static int budget;
     private static int[][] timeCostTable;
+    private static int result;
 
     public static void main(String[] args) {
         parseInput();
-        for (int i = 0; i < timeCostTable.length; i++) {
-            System.out.printf("time: %d cost: %d", timeCostTable[i][0], timeCostTable[i][1]);
-            System.out.println();
-        }
-        System.out.println(budget);
+        System.out.println(getMaxMinutes());
     }
 
     /**
@@ -53,5 +50,26 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Calculates the maximum amount of minutes Muzzy can afford
+     * This method uses Knapsack algorithm done with DP
+     * @return Maximum amount of minutes Muzzy can afford
+     */
+    public static int getMaxMinutes() {
+        int[][] bufferTable = new int [timeCostTable.length+1][budget+1];
+
+        for(int i = 1; i < bufferTable.length; i++)
+            for (int j = 1; j < bufferTable[i].length; j++){
+                if (timeCostTable[i-1][1] <= j) {
+                    int overflow = j - timeCostTable[i-1][1];
+                    int possibleValue = timeCostTable[i-1][0] + bufferTable[i-1][overflow];
+                    bufferTable[i][j] = Math.max(bufferTable[i-1][j], possibleValue);
+                } else {
+                    bufferTable[i][j] = bufferTable[i-1][j];
+                }
+            }
+        return bufferTable[bufferTable.length-1][bufferTable[bufferTable.length-1].length-1];
     }
 }
