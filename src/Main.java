04aec3d6d298a89cf253721c4a9a5e32db1edc8e
Bulat Khabirov <khabiroff_b@me.com>
2017-02-27 01:28:59 +0300

    Finished implementation + Removed logging + Added writing to file method + Little main algorithm fixes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.FileReader;
+import java.io.*;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -6,11 +6,10 @@ import java.util.Scanner;
 public class Main {
     private static int budget;
     private static int[][] timeCostTable;
-    private static int result;
 
     public static void main(String[] args) {
         parseInput();
-        System.out.println(getMaxMinutes());
+        writeString(String.valueOf(getMaxMinutes()), "output.txt");
     }
 
     /**
@@ -18,7 +17,7 @@ public class Main {
      * @param timeString A string of format HH:mm to parse
      * @return Time value of given string in minutes
      */
-    public static int parseTimeString(String timeString) {
+    private static int parseTimeString(String timeString) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("k:mm");
         LocalTime time = LocalTime.parse(timeString, formatter);
         return time.getMinute() + 60*time.getHour();
@@ -27,12 +26,12 @@ public class Main {
     /**
      * Fills the table and budget from "input.txt" file
      */
-    public static void parseInput() {
+    private static void parseInput() {
         try {
             Scanner scanner = new Scanner(new FileReader("input.txt"));
             String[] input = scanner.nextLine().split(" ");
             timeCostTable = new int[input.length/2][2];
-            System.out.println("begin parse");
+
             int i = 0;
             for (String element : input) {
                 if (i % 2 == 0) {
@@ -46,7 +45,6 @@ public class Main {
             }
 
             budget = (int)(Double.parseDouble(scanner.nextLine())*100);
-            System.out.println("ended");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,7 +55,7 @@ public class Main {
      * This method uses Knapsack algorithm done with DP
      * @return Maximum amount of minutes Muzzy can afford
      */
-    public static int getMaxMinutes() {
+    private static int getMaxMinutes() {
         int[][] bufferTable = new int [timeCostTable.length+1][budget+1];
 
         for(int i = 1; i < bufferTable.length; i++)
@@ -65,11 +63,28 @@ public class Main {
                 if (timeCostTable[i-1][1] <= j) {
                     int overflow = j - timeCostTable[i-1][1];
                     int possibleValue = timeCostTable[i-1][0] + bufferTable[i-1][overflow];
+
                     bufferTable[i][j] = Math.max(bufferTable[i-1][j], possibleValue);
                 } else {
                     bufferTable[i][j] = bufferTable[i-1][j];
                 }
             }
         return bufferTable[bufferTable.length-1][bufferTable[bufferTable.length-1].length-1];
+    }
+
+    /**
+     * @param stringToWrite A string to write
+     * @param fileName A full name of the file to write the given string to
+     */
+    private static void writeString(String stringToWrite, String fileName) {
+        try {
+            try (Writer writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(fileName), "ascii"))) {
+                writer.write(stringToWrite);
+            }
+        } catch (IOException ex) {
+            System.out.print(ex.getMessage());
+        }
     }
 }
